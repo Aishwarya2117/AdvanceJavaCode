42c35faa4cce1fa1e01049b4b6ad48cac1bd43de
Aishwarya Mhetre <mhetreaishwarya07@gmail.com>
2025-02-11 16:11:56 +0530

    added

--- Servlet2/src/main/java/com/app/backend/ServletHttp.java
+++ Servlet2/src/main/java/com/app/backend/ServletHttp.java
@@ -1,6 +1,7 @@
 package com.app.backend;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +14,16 @@ public class ServletHttp  extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		super.doGet(req, resp);
-		System.out.println("I m in doGet() method");
+		//System.out.println("I m in doGet() method");
+		resp.setContentType("text/html");
+		resp.getWriter().println("<h2>Hello! This is a GET response fromMyServlet.</h2>");
+		
 	}
+	@Override
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        response.setContentType("text/html");
+	        PrintWriter out =response.getWriter();
+	        out.print("i m in do method for browser");
+	        //("<h2>Hello! This is a POST response from MyServlet.</h2>");
+	    }
 }
