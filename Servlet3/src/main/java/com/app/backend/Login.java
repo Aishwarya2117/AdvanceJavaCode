d8dfd06e75623afd5c89faba5e9561e8ecbafa4f
Aishwarya Mhetre <mhetreaishwarya07@gmail.com>
2025-02-12 09:57:51 +0530

    new

---
+++ Servlet3/src/main/java/com/app/backend/Login.java
@@ -0,0 +1,26 @@
+package com.app.backend;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login  extends HttpServlet{
+
+	public Login() {
+		super();
+	}
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("I m in Service method");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		System.out.println("In doGet of Login");
+	}
+}
