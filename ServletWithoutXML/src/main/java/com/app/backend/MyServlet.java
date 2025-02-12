236041e0fe666781e8de77ca28c4c27513e674a2
Aishwarya Mhetre <mhetreaishwarya07@gmail.com>
2025-02-12 20:34:38 +0530

    updated

---
+++ ServletWithoutXML/src/main/java/com/app/backend/MyServlet.java
@@ -0,0 +1,21 @@
+package com.app.backend;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{ 
+	
+	public MyServlet() {
+		super();
+	}
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+	}
+
+}
